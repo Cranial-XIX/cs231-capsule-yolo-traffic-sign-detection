e367d07680e9b246b525cec393dac593b6ebf252
VermicelliInCasserole <bliu24@jhu.edu>
2018-05-27 21:18:08 -0700

    change to spaces

--- config.py
+++ config.py
@@ -7,27 +7,27 @@ GTSDB = 'data/GTSDB'
 
 # data directory
 data_dir = {
-	'cnn' 			  : GTSRB,
-	'capsule' 		  : GTSRB,
-	'darknet_d' 	  : GTSDB,
-	'darknet_r' 	  : GTSDB,
-	'darkcapsule' 	  : GTSDB,
+    'cnn'             : GTSRB,
+    'capsule'         : GTSRB,
+    'darknet_d'       : GTSDB,
+    'darknet_r'       : GTSDB,
+    'darkcapsule'     : GTSDB,
 }
 
 # model directory
 model_dir = {
-	'cnn' 			  : 'experiments/cnn',
-	'capsule' 		  : 'experiments/capsule',
-	'darknet_d' 	  : 'experiments/darknet_d',
-	'darknet_r' 	  : 'experiments/darknet_r',
-	'darkcapsule' 	  : 'experiments/darkcapsule',
+    'cnn'             : 'experiments/cnn',
+    'capsule'         : 'experiments/capsule',
+    'darknet_d'       : 'experiments/darknet_d',
+    'darknet_r'       : 'experiments/darknet_r',
+    'darkcapsule'     : 'experiments/darkcapsule',
 }
 
 # input shape
 input_shape = {
-	'cnn' 			  : (3, 32, 32),
-	'capsule' 		  : (3, 32, 32),
-	'darknet_d' 	  : (3, 32, 32),
-	'darknet_r' 	  : (3, 32, 32),
-	'darkcapsule' 	  : (3, 32, 32),
+    'cnn'             : (3, 32, 32),
+    'capsule'         : (3, 32, 32),
+    'darknet_d'       : (3, 32, 32),
+    'darknet_r'       : (3, 32, 32),
+    'darkcapsule'     : (3, 32, 32),
 }
