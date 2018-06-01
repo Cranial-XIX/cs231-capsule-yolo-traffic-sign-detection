ecbc988f684a4e37158ac556e96e341f9f8840ce
Peng Li <lipengshopping@gmail.com>
2018-05-31 22:25:23 -0700

    add comment

--- predict_fns.py
+++ predict_fns.py
@@ -7,7 +7,23 @@ import torchvision.transforms as transforms
 import torch
 from loss_fns import cnn_loss, capsule_loss, dark_loss, darkcapsule_loss
 
-def dark_pred(images, model, model_dir, restore, params, y_true = None):
+def dark_pred(images, model, model_dir, restore, params, y_true = None, is_metric = False):
+	""" Darknet prediction 
+    
+    Args:
+    	images: list of images, of shape (n_images, ). Image size can be different.
+    	model: darknet model.
+    	model_dir: directory where weights are saved.
+    	restore: "last" or "best"
+    
+    Return:
+        If is metric: 
+        	y_pred: of shape(n_images, n_grid, n_grid, 5B + C)
+		If not metric:
+			output_images: images with boxes, of shape (n_images, )
+			output_crops: crops of boxes, of shape (n_boxes, capsule_input, capsule_input, 3)
+			image_indices: (n_boxes, )
+	"""
 	restore_path = os.path.join(model_dir, restore + '.pth.tar')
 	print("Restoring parameters from {}".format(restore_path))
 	utils.load_checkpoint(restore_path, model, params)
@@ -23,24 +39,18 @@ def dark_pred(images, model, model_dir, restore, params, y_true = None):
 	x = x.to(device=params.device, dtype=torch.float32)
 	y_pred = model(x)
 
+	if is_metric:
+		return y_pred
+
 	if y_true is not None:
 		y_true = torch.from_numpy(y_true).to(device=params.device)
 		loss = dark_loss(y_pred, y_true, params)
 		print("loss:", loss)
+
 	y_pred = y_pred.data.numpy()
-	
 	image_indices, xy, classes = utils.y_to_boxes_vec(y_pred, image_hw, params.n_classes, conf_th = 0.5)
 	output_images, crops_bch = plot.draw_boxes_vec(images, image_indices, xy, classes)
 
 	capsule_input = (params.capsule_input, params.capsule_input)
-
 	output_crops = np.array([cv2.resize(crop, capsule_input) for crops in crops_bch for crop in crops])
-	print(output_crops.shape)
-	
-	for i, img in enumerate(output_images):
-		cv2.imshow('image ' + str(i), img)
-	for i, crop in enumerate(output_crops):
-		cv2.imshow('crop ' + str(i), crop)
-
-	cv2.waitKey(0)
-	return output_images
+	return output_images, output_crops, image_indices
