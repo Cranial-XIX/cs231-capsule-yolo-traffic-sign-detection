294da36a9bddecebe3992133050f35ca61ec03b1
Vincentli8540 <36247164+Vincentli8540@users.noreply.github.com>
2018-06-04 19:38:23 -0700

    data_augmentation
    1. change darknet_r/params add_signs:1
    2. add data_augmentation

--- build_data.py
+++ build_data.py
@@ -71,6 +71,7 @@ def gtsdb(params, aug_size=0, root=config.GTSDB):
     classes = raw_data[:, 5].astype(int)
 
     X, Y = [], []
+    X_aug, Y_aug = [], []
     conflict_count = 0
 
     for i in trange(data_size):
@@ -103,24 +104,66 @@ def gtsdb(params, aug_size=0, root=config.GTSDB):
 
         Y.append(y)
 
+        # Data Augmentation
+        for itr in range(aug_size):
+            x_aug, y_aug = gtsdb_aug_(params, image, \
+                box_coords[indices], classes[indices])
+
+            X_aug.append(x_aug)
+            Y_aug.append(y_aug)
+
+
+    X_aug, Y_aug = np.array(X_aug).squeeze(), np.array(Y_aug).squeeze()
+
     X, Y = np.array(X), np.array(Y)
-    X, Y = utils.shuffle(X, Y)
+    X, Y, X_aug, Y_aug = utils.shuffle_aug(X, Y, X_aug, Y_aug)
+
+    # X, Y = np.array(X), np.array(Y)
+    # X, Y = utils.shuffle(X, Y)
+
+    # split = data_size // 10
+    # X_ev = X[:split]
+    # Y_ev = Y[:split]
+    # X_te = X[split:2*split]
+    # Y_te = Y[split:2*split]
+
+    # X_tr = X[2*split:]
+    # Y_tr = Y[2*split:]
+    
+    # # data augmentation
+    # if aug_size != 0:
+    #     print('data augmentationg begins:')
+    #     X_aug, Y_aug = gtsdb_aug(params, aug_size)
+    #     X_tr = np.concatenate((X_tr, X_aug), axis=0)
+    #     Y_tr = np.concatenate((Y_tr, Y_aug), axis=0)
+    print('Augmentation shape:')
+    print(X_aug.shape)
+    print(Y_aug.shape)
 
     split = data_size // 10
+    X_ev_aug = X_aug[:split]
+    Y_ev_aug = Y_aug[:split]
+    X_te_aug = X_aug[split:2*split]
+    Y_te_aug = Y_aug[split:2*split]
+    X_tr_aug = X_aug[2*split:]
+    Y_tr_aug = Y_aug[2*split:]
+    
     X_ev = X[:split]
     Y_ev = Y[:split]
     X_te = X[split:2*split]
     Y_te = Y[split:2*split]
-
     X_tr = X[2*split:]
     Y_tr = Y[2*split:]
-    
-    # data augmentation
-    if aug_size != 0:
-        print('data augmentationg begins:')
-        X_aug, Y_aug = gtsdb_aug(params, aug_size)
-        X_tr = np.concatenate((X_tr, X_aug), axis=0)
-        Y_tr = np.concatenate((Y_tr, Y_aug), axis=0)
+
+    X_small = X[0:2]
+    Y_small = Y[0:2]
+
+    X_tr = np.concatenate((X_tr, X_tr_aug),axis=0)
+    Y_tr = np.concatenate((Y_tr, Y_tr_aug),axis=0)
+    X_ev = np.concatenate((X_ev, X_ev_aug),axis=0)
+    Y_ev = np.concatenate((Y_ev, Y_ev_aug),axis=0)
+    X_te = np.concatenate((X_te, X_te_aug),axis=0)
+    Y_te = np.concatenate((Y_te, Y_te_aug),axis=0)
 
     pickle.dump((X_tr, Y_tr), open(root+'/train.p', 'wb'))
     pickle.dump((X_ev, Y_ev), open(root+'/eval.p', 'wb'))
@@ -139,167 +182,125 @@ def gtsdb(params, aug_size=0, root=config.GTSDB):
     print('Number of boxes:', box_coords.shape[0])
     print('Conflict count:', conflict_count)
 
-def gtsdb_aug(params, output_size, root = 'data'):
+def gtsdb_aug_(params, image, box_xy, classes):
+    class_dir = 'data/GTSRB/Images/'
 
-    # directory path
-    data_dir = root + '/GTSDB/raw_GTSDB/'
-    class_dir = root + '/GTSRB/Images/'
-    
-
-    # files from raw_GTSDB
-    image_files = [f for f in os.listdir(data_dir) if f.endswith('.ppm')]
-    data_size = len(image_files)
-    raw_data = np.loadtxt(data_dir + '/gt.txt', delimiter = ';', dtype= str)
-    image_names = raw_data[:, 0]
-    box_coords = raw_data[:, 1:5].astype(float)
-    classes = raw_data[:, 5].astype(int)
-
-    # some pre-defined parameters
     add_signs = params.add_signs
     resized_hw = [params.darknet_input, params.darknet_input]
 
     # two data loaders
-    X, Y = [], []
+    X_aug, Y_aug = [], []
 
-    for itr_image in trange(output_size):
+    # extract the existing signs' bounding boxes
+    signs_list = {}
+    num_orign_signs = box_xy.shape[0]
 
-        # randomly select one image
-        orig_image_name = random.choice(os.listdir(data_dir))
-        while "ppm" not in orig_image_name: 
-            orig_image_name = random.choice(os.listdir(data_dir))
-        image = cv2.imread(os.path.join(data_dir, orig_image_name))
+    # occlude the existing and paste "add_signs" new signs
+    num_signs = num_orign_signs + add_signs
+    # num_signs = np.random.randint(num_orign_signs, max_signs+1)
 
-        # extract the existing signs' bounding boxes
-        signs_list = {}
-        indices = np.argwhere(image_names == orig_image_name).reshape(-1,)
-        num_orign_signs = len(indices)
-        box_xy = box_coords[indices]
+    # randomly select num_signs
+    for itr_sign in range(num_signs):
 
-        # occlude the existing and paste "add_signs" new signs
-        idx = 0
-        max_signs = num_orign_signs + add_signs
-        num_signs = np.random.randint(num_orign_signs, max_signs+1)
-
-        # randomly select num_signs
-        for itr_sign in range(num_signs):
-
-            # a class from 43 classes
+        # a class from 43 classes
+        class_name = random.choice(os.listdir(class_dir))
+        while "0" not in class_name: 
             class_name = random.choice(os.listdir(class_dir))
-            while "0" not in class_name: 
-                class_name = random.choice(os.listdir(class_dir))
 
-            # a sign from that class
+        # a sign from that class
+        sign_name = random.choice(os.listdir(class_dir + class_name + '/'))
+        while "ppm" not in sign_name: 
             sign_name = random.choice(os.listdir(class_dir + class_name + '/'))
-            while "ppm" not in sign_name: 
-                sign_name = random.choice(os.listdir(class_dir + class_name + '/'))
-            
-            # load sign bounding boxes
-            data_signs = np.loadtxt(class_dir +  class_name + '/GT-' + class_name \
-                + '.csv', delimiter = ';', dtype= str)[1:]
-            
-            selected = np.argwhere(data_signs == sign_name)[0][0]
-
-            box_coords_data = data_signs[selected, 1:8].astype(int)
-            #   key    height width startX startY endX endY  class
-            # "name"    0      1      2       3     4     5    6
-
-            signs_list[str(data_signs[selected,0])] = box_coords_data
-
-        # array y records new bounding boxes recording 
-        y = np.zeros((params.n_grid, params.n_grid, 5 + params.n_classes))
         
-        # perform data augmentation
-        for key in signs_list:
+        # load sign bounding boxes
+        data_signs = np.loadtxt(class_dir +  class_name + '/GT-' + class_name \
+            + '.csv', delimiter = ';', dtype= str)[1:]
+        
+        selected = np.argwhere(data_signs == sign_name)[0][0]
 
-            # one sign's info
-            single_sign = signs_list[key]
-            class_name = utils.get_image_name(single_sign[6])[:-4]
-            sign = cv2.imread(class_dir +  '/' + class_name + '/' + key)
+        box_coords_data = data_signs[selected, 1:8].astype(int)
+        #   key    height width startX startY endX endY  class
+        # "name"    0      1      2       3     4     5    6
 
-            # FROM this sign
-            fromX1, fromY1, fromX2, fromY2 = single_sign[2:6]
-            fromH, fromW = fromY2 - fromY1, fromX2 - fromX1
+        signs_list[str(data_signs[selected,0])] = box_coords_data
 
-            # 1. occlude existing signs
-            if idx < num_orign_signs:
-
-                # TO the selected image
-                toX1, toY1, toX2, toY2 = box_xy[idx].astype(int)
-                toH, toW= toY2 - toY1, toX2 - toX1 
-                ratioH, ratioW  = toH / fromH, toW/fromW
-                rescaleH, rescaleW = int(ratioH*fromH), int(ratioW*fromW) 
-
-                # resize the selected sign to fit into the space of existing signs              
-                resized_single_sign = \
-                    cv2.resize(sign[fromY1:fromY2, fromX1:fromX2], (toW, toH))
-
-                # paste
-                image[toY1:toY2, toX1:toX2] = resized_single_sign
-                
-                # record the new bounding boxes
-                # Note: box_xy is the same as the existing signs
-                new_box_xy = box_xy[idx].astype(int)
-                resized_box_xy = \
-                    utils.resize_box_xy(image.shape[0:2], resized_hw, new_box_xy)
-                box_cwh = utils.xy_to_cwh(resized_box_xy)
-                (xc, yc, w, h), (row, col) = \
-                    utils.normalize_box_cwh(resized_hw, params.n_grid, box_cwh)
-                y[row, col, 0:5] = [1, xc, yc, w, h]
-                y[row, col, 5 + single_sign[6]] = 1
-                
-                idx += 1
-            
-            # 2. add new signs
-            else:
-
-                # TO a random position of the selected image
-                X_start = np.random.randint(0, image.shape[1] -  single_sign[0])
-                Y_start = np.random.randint(0, image.shape[0] -  single_sign[1])
-                toX1, toY1, toX2, toY2 = X_start, Y_start, X_start+fromW, Y_start+fromH
-                toH, toH = toY2 - toY1, toX2 - toX1
-
-                # paste
-                image[toY1:toY2, toX1:toX2] = sign[fromY1:fromY2, fromX1:fromX2]
-               
-                # record the new bounding boxes
-                # Note: box_xy is the newly defined bounding boxes
-                new_box_xy = [toX1, toY1, toX2, toY2]
-                resized_box_xy = \
-                    utils.resize_box_xy(image.shape[0:2], resized_hw, new_box_xy)
-                box_cwh = utils.xy_to_cwh(resized_box_xy)
-                (xc, yc, w, h), (row, col) = \
-                    utils.normalize_box_cwh(resized_hw, params.n_grid, box_cwh)
-                y[row, col, 0:5] = [1, xc, yc, w, h]
-                y[row, col, 5 + single_sign[6]] = 1
-
-        resized_image = cv2.resize(image, (params.darknet_input, params.darknet_input))
-        Y.append(y)
-        X.append(resized_image)
-
-    X, Y = np.array(X), np.array(Y)
+    # array y records new bounding boxes recording 
+    y = np.zeros((params.n_grid, params.n_grid, 5 + params.n_classes))
     
-    print('data augmentation is done')
-    print('X shape:')
-    print(X.shape)
-    print('Y shape:')
-    print(Y.shape)
+    idx = 0
 
-    return X, Y
-    # # copied from gtsdb()
-    # X, Y = utils.shuffle(X, Y)
-    # split = X.shape[0] // 10
-    # X_ev = X[:split]
-    # Y_ev = Y[:split]
-    # X_te = X[split:2*split]
-    # Y_te = Y[split:2*split]
-    # X_tr = X[2*split:]
-    # Y_tr = Y[2*split:]
+    # perform data augmentation
+    for key in signs_list:
 
-    # pickle.dump((X_tr, Y_tr), open(root+'/GTSDB/train.p', 'wb'))
-    # pickle.dump((X_ev, Y_ev), open(root+'/GTSDB/eval.p', 'wb'))
-    # pickle.dump((X_te, Y_te), open(root+'/GTSDB/test.p', 'wb'))
+        # one sign's info
+        single_sign = signs_list[key]
+        class_name = utils.get_image_name(single_sign[6])[:-4]
+        sign = cv2.imread(class_dir +  '/' + class_name + '/' + key)
 
+        # FROM this sign
+        fromX1, fromY1, fromX2, fromY2 = single_sign[2:6]
+        fromH, fromW = fromY2 - fromY1, fromX2 - fromX1
 
+        # 1. occlude existing signs
+        if idx < num_orign_signs:
+
+            # TO the selected image
+            toX1, toY1, toX2, toY2 = box_xy[idx].astype(int)
+            toH, toW= toY2 - toY1, toX2 - toX1 
+            ratioH, ratioW  = toH / fromH, toW/fromW
+            rescaleH, rescaleW = int(ratioH*fromH), int(ratioW*fromW) 
+
+            # resize the selected sign to fit into the space of existing signs              
+            resized_single_sign = \
+                cv2.resize(sign[fromY1:fromY2, fromX1:fromX2], (toW, toH))
+
+            # paste
+            image[toY1:toY2, toX1:toX2] = resized_single_sign
+            
+            # record the new bounding boxes
+            # Note: box_xy is the same as the existing signs
+            new_box_xy = box_xy[idx].astype(int)
+            resized_box_xy = \
+                utils.resize_box_xy(image.shape[0:2], resized_hw, new_box_xy)
+            box_cwh = utils.xy_to_cwh(resized_box_xy)
+            (xc, yc, w, h), (row, col) = \
+                utils.normalize_box_cwh(resized_hw, params.n_grid, box_cwh)
+            y[row, col, 0:5] = [1, xc, yc, w, h]
+            y[row, col, 5 + single_sign[6]] = 1
+            
+            idx += 1
+        
+        # 2. add new signs
+        else:
+
+            # TO a random position of the selected image
+            X_start = np.random.randint(0, image.shape[1] -  single_sign[0])
+            Y_start = np.random.randint(0, image.shape[0] -  single_sign[1])
+            toX1, toY1, toX2, toY2 = X_start, Y_start, X_start+fromW, Y_start+fromH
+            toH, toH = toY2 - toY1, toX2 - toX1
+
+            # paste
+            image[toY1:toY2, toX1:toX2] = sign[fromY1:fromY2, fromX1:fromX2]
+           
+            # record the new bounding boxes
+            # Note: box_xy is the newly defined bounding boxes
+            new_box_xy = [toX1, toY1, toX2, toY2]
+            resized_box_xy = \
+                utils.resize_box_xy(image.shape[0:2], resized_hw, new_box_xy)
+            box_cwh = utils.xy_to_cwh(resized_box_xy)
+            (xc, yc, w, h), (row, col) = \
+                utils.normalize_box_cwh(resized_hw, params.n_grid, box_cwh)
+            y[row, col, 0:5] = [1, xc, yc, w, h]
+            y[row, col, 5 + single_sign[6]] = 1
+
+    resized_image = cv2.resize(image, (params.darknet_input, params.darknet_input))
+    Y_aug.append(y)
+    X_aug.append(resized_image)
+
+    # X_aug, Y_aug = np.array(X_aug), np.array(Y_aug)
+
+    return X_aug, Y_aug
+    
 if __name__ == "__main__":
     args = parser.parse_args()
     np.random.seed(0)
