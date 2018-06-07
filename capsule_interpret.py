384c3e1c1718fa29470524f40d6db77e5c2a1047
VermicelliInCasserole <bliu24@jhu.edu>
2018-06-06 23:47:10 -0700

    finish capsule interpretation

--- capsule_interpret.py
+++ capsule_interpret.py
@@ -24,24 +24,27 @@ def show(a, b):
     cv2.imshow('recon', (b+1)/2)
     cv2.waitKey(0)
 
-param_dir = './experiments/capsule'
+param_dir = './experiments/capsule/'
 
 params = load_params(param_dir)
-x, y = pickle.load(open('./data/GTSDB/eval.p', 'rb'))
+x, y = pickle.load(open('./data/GTSRB/eval.p', 'rb'))
 
-i = 0
+i = 90
 xxx = x[i]
 yy = y[i]
 
-print(np.min(xxx), np.max(xxx))
-print(xxx)
 '''
+for i in range(100):
+    cv2.imwrite("img/"+str(i) +".png", x[i] * 128.0 + 128)
+'''
+
+
 xx = torch.from_numpy(xxx).float().unsqueeze(0).permute(0, 3, 1, 2).to(device=params.device)
 yy = torch.from_numpy(np.array(yy).reshape(1,)).to(device=params.device)
 
 
 model = CapsuleNet(params).to(device=params.device)
-model_dir = './experiments/capsule1.0'
+model_dir = './experiments/capsule1'
 restore_path = os.path.join(model_dir, 'best.pth.tar')
 print("Restoring parameters from {}".format(restore_path))
 utils.load_checkpoint(restore_path, model, params)
@@ -53,9 +56,13 @@ x = model.primary_capsules(x)
 x = model.traffic_sign_capsules(x).squeeze()
 
 t = torch.gather(x.unsqueeze(0), 1, yy.repeat(16, 1).t().unsqueeze(1)).squeeze()
-decoded = model.decoder(t)
-decoded = decoded.permute(0, 2, 3, 1).squeeze()
+cc = np.arange(11) * 0.05 - 0.25
 
-show(xxx, decoded.data.numpy())
-'''
-    
+cv2.imwrite("img/orig.png", xxx * 128.0 + 128)
+for v in range(16):
+    for i, c in enumerate(cc):
+        t[v] = t[v] + c
+        decoded = model.decoder(t)
+        decoded = decoded.permute(0, 2, 3, 1).squeeze()
+        cv2.imwrite("img/"+str(v)+"-"+str(i) +".png", decoded.data.numpy() * 128.0 + 128)
+        t[v] = t[v] - c
